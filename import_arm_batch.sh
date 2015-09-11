05d6dc3757f4ae6d298351c69904c80027f8ff0d
James Jones <jamjon3@gmail.com>
2015-09-11 15:31:46 -0400

    Sample script to batch imports together

---
+++ import_arm_batch.sh
@@ -0,0 +1,7 @@
+# php index.php roles ~/Documents/arm_data/FAST/armImport-FAST-20150911/FAST_roles
+# php index.php accounts ~/Documents/arm_data/FAST/armImport-FAST-20150911/FAST_accounts
+# php index.php mapping ~/Documents/arm_data/FAST/armImport-FAST-20150911/FAST_accounts_roles.json
+
+php index.php roles ~/Documents/arm_data/GEMS/armImport-GEMS-20150911/GEMS_roles
+php index.php accounts ~/Documents/arm_data/GEMS/armImport-GEMS-20150911/GEMS_accounts
+php index.php mapping ~/Documents/arm_data/GEMS/armImport-GEMS-20150911/GEMS_accounts_roles.json
