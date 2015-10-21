f4902ae0bf0fec0c61552665a98d159d2ce1db99
James Jones <jamjon3@gmail.com>
2015-10-21 13:09:25 -0400

    Added backup:export command

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+rm -rf bin
 echo "**************************************************************************"
 echo "* Updating composer for the armimport package                            *"
 echo "**************************************************************************"
 composer update
-mkdir -p dist
+mkdir -p bin
 echo "**************************************************************************"
-echo "* Building the dist/armimport.phar file                                  *"
+echo "* Building the bin/armimport.phar file                                  *"
 echo "**************************************************************************"
 vendor/bin/box build
 echo "**************************************************************************"
---
+++ src/DumpArmCommand.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace USF\IdM;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+/**
+ * Description of DumpArmCommand
+ *
+ * @author james
+ */
+class DumpArmCommand extends Command {
+    protected function configure() {
+        $this->setName("backup:export")
+            ->setDescription("Exports ARM mongo data to zipped dump")
+            ->addOption('out',null,InputOption::VALUE_REQUIRED,'What is the folder used for output?',false)
+            ->setHelp("Usage: <info>php console.php backup:export <env></info>");
+    }
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $tempfile = \tempnam(\sys_get_temp_dir(), '');
+        if (file_exists($tempfile)) { unlink($tempfile); }
+        mkdir($tempfile);
+        mkdir($tempfile."/dump");
+        $results = [];
+        $dump_cmd = "mongodump --db arm --collection ";
+        $results[] = shell_exec($dump_cmd."roles --out ".$tempfile."/dump");
+        $results[] = shell_exec($dump_cmd."accounts --out ".$tempfile."/dump");
+        // Initialize archive object
+        $zip = new \ZipArchive;        
+        if($input->getOption('out')) {
+            $zipPath = $input->getOption('out')."/arm_backup.zip";
+        } else {
+            $zipPath = \getcwd()."/arm_backup.zip";
+        }
+        if($zip->open($zipPath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) !== true) {
+          exit('cannot create zip');            
+        }
+        // Create recursive directory iterator
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($tempfile."/dump", \FilesystemIterator::SKIP_DOTS), 
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+        $files_to_delete = [];
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                // Get real path for current file            
+                $filePath = $file->getRealPath();
+                // Add current file to archive
+                $zip->addFile($filePath,\substr($name, strlen($tempfile)));
+                // add file to delete queue
+                $files_to_delete[] = $filePath;
+            }
+        }
+        
+        $results[] = "Zip status: ".$zip->getStatusString();
+        $zip->close();
+        $this->delete_files($tempfile);
+        $results[] = "Temp directory removed";
+        $results[] = $zipPath." created";
+        // mongodump --db arm --collection roles
+        // $command = "mongodump --db " . $this->database . " --out " . $this->current_dump_path;
+        $output->writeln($results);
+        
+    }
+    /**
+     * php delete function that deals with directories recursively
+     */
+    private function delete_files($target) {
+        if (is_dir($target)) {
+            $files = \glob($target . '*', GLOB_MARK); //GLOB_MARK adds a slash to directories returned
+
+            foreach ($files as $file) {
+                $this->delete_files($file);
+            }
+            if(\file_exists($target)) { 
+                \rmdir($target);
+            }
+        } elseif (is_file($target)) {
+            \unlink($target);
+        }
+    }
+
+}
